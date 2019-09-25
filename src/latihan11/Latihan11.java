c785f8d45d913058ef1da60e803b30ef3fc7fc03
User <User@DESKTOP-2RC2UP5>
2019-09-25 08:52:08 +0700

    first commit

---
+++ src/latihan11/Latihan11.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan11;
+
+/**
+ *
+ * @author User
+ * NAMA:REZA PRATAMA
+ * KELAS:PBO2
+ * NIM:10118055
+ * Deskripsi:program ini berisi program untuk menampilkan
+ * jenis jenis data bilangan bulat
+ */
+public class Latihan11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i=3456764;
+        int iMinus=-i;
+        System.out.println("i:"+i);
+                System.out.printf("%%d:%d%n",i);
+                        System.out.printf("%%10d:%10d%n",i);
+                               System.out.printf("%%+10d:%+10d%n",i);
+        System.out.printf("%%+10d:%+10d%n",iMinus);
+              System.out.printf("%%,10d:%,10d%n",i);
+         System.out.printf("%%-10d:%-10d%n",i);
+         double f=5675482.982;
+         System.out.println("f:"+f);
+         System.out.printf("%%f:%f%n",f);
+         System.out.printf("%%.2f:%.2f%n",f);
+         System.out.printf("%%12.2f:%12.2f%n",f);
+         System.out.printf("%%,12.2f:%,12.2f%n",f);
+    }
+    
+}
